58a760e98bdb8889309795a2a39a4c747879f147
QJUXUQ <justiina.laitinen@hotmail.com>
2024-03-26 16:19:41 +0200

    02 BankAssignment tehtynä

--- 01_BasicExcercises/Bank/BankAccount.cs
+++ 01_BasicExcercises/Bank/BankAccount.cs
@@ -5,23 +5,22 @@
     /// </summary>
     public class BankAccount
     {
-        private readonly string m_customerName;
+       
         private double m_balance;
 
         public const string DebitAmountExceedsBalanceMessage = "Debit amount exceeds balance";
         public const string DebitAmountLessThanZeroMessage = "Debit amount is less than zero";
+        public const string BalanceAmountLessThanNeededMessage = "Bank account does not have enough value for this transaction";
+        public const string CreditAmountLessThanZeroMessage = "Credit amount is less than zero";
         private BankAccount() { }
 
-        public BankAccount(string customerName, double balance)
+        public BankAccount(double balance)
         {
-            m_customerName = customerName;
+            
             m_balance = balance;
         }
 
-        public string CustomerName
-        {
-            get { return m_customerName; }
-        }
+       
 
         public double Balance
         {
@@ -47,19 +46,33 @@
         {
             if (amount < 0)
             {
-                throw new ArgumentOutOfRangeException("amount");
+                throw new ArgumentOutOfRangeException("amount",amount,CreditAmountLessThanZeroMessage);
             }
 
             m_balance += amount;
         }
 
+        public static void SendMoney(BankAccount sendingAccount,BankAccount receivingAccount, double sendingAmount) 
+        {
+            if (sendingAccount.m_balance > sendingAmount)
+            {
+                sendingAccount.m_balance -= sendingAmount;
+                receivingAccount.m_balance += sendingAmount;
+            }
+            else 
+            {
+                throw new ArgumentOutOfRangeException(BalanceAmountLessThanNeededMessage);
+            }
+        }
+
         public static void Main()
         {
-            BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
+            BankAccount ba = new BankAccount( 11.99);
 
             ba.Credit(5.77);
             ba.Debit(11.22);
             Console.WriteLine("Current balance is ${0}", ba.Balance);
         }
+
     }
 }
--- 01_BasicExcercises/BankAccountTest/UnitTest1.cs
+++ 01_BasicExcercises/BankAccountTest/UnitTest1.cs
@@ -1,3 +1,4 @@
+using Bank;
 using BankAccountNS;
 namespace BankTests
 {
@@ -11,13 +12,15 @@ namespace BankTests
             double beginningBalance = 11.99;
             double debitAmount = 4.55;
             double expected = 7.44;
-            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+            BankCustomer customer = new BankCustomer("Mr. Bryan Walton");
+            customer.AddAccount(beginningBalance);
+            
 
             //Act
-            account.Debit(debitAmount);
+            customer.CustomerAccount[0].Debit(debitAmount);
 
             //Assert
-            double actual = account.Balance;
+            double actual = customer.CustomerAccount[0].Balance;
             Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
         }
 
@@ -27,18 +30,20 @@ namespace BankTests
             //Arrange
             double beginningBalance = 11.99;
             double debitAmount = -100.00;
-            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+            BankCustomer customer = new BankCustomer("Mr. Bryan Walton");
+            customer.AddAccount(beginningBalance);
 
             //Act and Assert
             try 
             {
-                account.Debit(debitAmount);
-            }catch (System.ArgumentOutOfRangeException e) 
+                customer.CustomerAccount[0].Debit(debitAmount);
+            }
+            catch (System.ArgumentOutOfRangeException e) 
             {
                 StringAssert.Contains(e.Message, BankAccount.DebitAmountLessThanZeroMessage);
                 return;
             }
-            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => account.Debit(debitAmount));
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => customer.CustomerAccount[0].Debit(debitAmount));
         }
 
 
@@ -48,12 +53,13 @@ namespace BankTests
             //Arrange
             double beginningBalance = 11.99;
             double debitAmount = 20.00;
-            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+            BankCustomer customer = new BankCustomer("Mr. Bryan Walton");
+            customer.AddAccount(beginningBalance);
 
             //Act
             try 
             {
-                account.Debit(debitAmount);
+                customer.CustomerAccount[0].Debit(debitAmount);
             } catch (System.ArgumentOutOfRangeException e) 
             {
                 //Assert
@@ -70,12 +76,231 @@ namespace BankTests
             double beginningBalance = 11.99;
             double creditAmount = 20;
             double expected =31.99;
-            BankAccount account = new BankAccount("Mr. Bryan Walton",beginningBalance);
-            account.Credit(creditAmount);
-            double actual = account.Balance;
+            BankCustomer customer = new BankCustomer("Mr. Bryan Walton");
+            customer.AddAccount(beginningBalance);
+            customer.CustomerAccount[0].Credit(creditAmount);
+            double actual = customer.CustomerAccount[0].Balance;
             Assert.AreEqual(expected, actual, 0.001);
+        }
+        [TestMethod]
+        public void Credit_IsNotWorking()
+        {
+            double beginningBalance = 11.99;
+            double creditAmount = -20;
+            
+            BankCustomer customer = new BankCustomer("Mr. Bryan Walton");
+            customer.AddAccount(beginningBalance);
+            try
+            {
+                customer.CustomerAccount[0].Credit(creditAmount);
+            }
+            catch (System.ArgumentOutOfRangeException e)
+            {
+                //Assert
+                StringAssert.Contains(e.Message, BankAccount.CreditAmountLessThanZeroMessage);
+                return;
+            }
+            Assert.Fail("The expection was not thrown.");
+        }
+
+        [TestMethod]
+        public void Account_IsRemoved() 
+        {
+            double beginningBalance = 11.99;
+            BankCustomer customer = new BankCustomer("Mr. Bryan Walton");
+            customer.AddAccount(beginningBalance);
+            customer.RemoveAccount(0);
+            int actual=customer.Accounts.Count();
+            int expected=0;
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void Account_IsNonExcisting() 
+        {
+            BankCustomer customer = new BankCustomer("Mr. Bryan Walton");
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() =>customer.RemoveAccount(0));
+        }
+        
+        [TestMethod]
+        public void Add_TwoAccounts() 
+        {
+            double beginningBalance = 11.99;
+            BankCustomer customer = new BankCustomer("Mr. Bryan Walton");
+            customer.AddAccount(beginningBalance);
+            customer.AddAccount(beginningBalance);
+            int expected = 2;
+            int actual= customer.Accounts.Count();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Transaction_Sucess() 
+        {
+            double beginningBalance = 11.99;
+            BankCustomer customer = new BankCustomer("Mr. Bryan Walton");
+            
+            customer.AddAccount(beginningBalance);
+            customer.AddAccount(beginningBalance);
+
+            double expectedAmountLeft =10.00;
+            double expectedAmountReceived = 13.98;
+            double takenAmount = 1.99;
+            
+
+            try 
+            {
+                BankAccount.SendMoney(customer.CustomerAccount[0], customer.CustomerAccount[1], takenAmount);
+            } catch (Exception ex)
+            {
+                StringAssert.Contains(ex.Message, BankAccount.BalanceAmountLessThanNeededMessage);
+                return;
+            }
+            double actual = customer.CustomerAccount[0].Balance;
+            double actual2 = customer.CustomerAccount[1].Balance;
+
+            Assert.AreEqual(expectedAmountLeft,actual);
+            Assert.AreEqual(expectedAmountReceived,actual2);
+            
+            
+        }
+
+        [TestMethod]
+        public void Transaction_Fail()
+        {
+            double beginningBalance = 0.99;
+            BankCustomer customer = new BankCustomer("Mr. Bryan Walton");
+
+            customer.AddAccount(beginningBalance);
+            customer.AddAccount(beginningBalance);
+
+            double takenAmount = 1.99;
+
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => BankAccount.SendMoney(customer.CustomerAccount[0], customer.CustomerAccount[1], takenAmount));
+        }
+
+        [TestMethod]
+
+        public void CreatingClient_FromBeginning_ToEnd() 
+        {
+            //asiakkaiden luonti
+
+            double beginningbalance = 11.99;
+            BankCustomer customer = new BankCustomer("Mr. Bryan Walton"); //kaksi uutta asiakasta
+            customer.AddAccount(beginningbalance); //yhdell‰ asiakkaalla kolme tili‰
+            customer.AddAccount(beginningbalance);
+            customer.AddAccount(beginningbalance);
+            BankCustomer customer2 = new BankCustomer("Ms. Marge Willow");
+            customer2.AddAccount(beginningbalance);
+
+            //tarkistetaan, ett‰ on luotu kolme tili‰ ensimm‰iselle asiakkaalle
+            double expected = 3;
+            double actual = customer.Accounts.Count();
+            Assert.AreEqual(expected, actual);
+
+            //tilin poisto
+            customer.RemoveAccount(2); //poistetaan kolmannes tili mik‰ luotiin, j‰‰ kaksi
+            actual = customer.Accounts.Count();
+            expected = 2;
+            Assert.AreEqual(expected, actual);
+
+            //debitin toiminta
+            double debitAmount = 4.55;
+            expected = 7.44;
+            customer.CustomerAccount[0].Debit(debitAmount);
+            actual = customer.CustomerAccount[0].Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+            
+            //debit on v‰hemm‰n kuin 0
+
+            debitAmount = -100.00;
+            try
+            {
+                customer.CustomerAccount[0].Debit(debitAmount);
+            }
+            catch (System.ArgumentOutOfRangeException e)
+            {
+                StringAssert.Contains(e.Message, BankAccount.DebitAmountLessThanZeroMessage);
+                
+            }
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => customer.CustomerAccount[0].Debit(debitAmount));
+
+            //debit enemm‰n kuin saatavilla
+            debitAmount = 20.00;
+            try
+            {
+                customer.CustomerAccount[0].Debit(debitAmount);
+            }
+            catch (System.ArgumentOutOfRangeException e)
+            {
+                //Assert
+                StringAssert.Contains(e.Message, BankAccount.DebitAmountExceedsBalanceMessage);
+                
+            }
+
+            //Creditin toiminta:
+
+            //k‰ytet‰‰n toista asiakasta ja h‰nen yht‰ tili‰‰n
+
+            double creditAmount = 20;
+            expected = 31.99;
+            customer2.CustomerAccount[0].Credit(creditAmount);
+            actual = customer2.CustomerAccount[0].Balance;
+            Assert.AreEqual(expected, actual, 0.001);
+
+            //Credit v‰hemm‰n kuin nolla
+            creditAmount = -20;
+            try
+            {
+                customer2.CustomerAccount[0].Credit(creditAmount);
+            }
+            catch (System.ArgumentOutOfRangeException e)
+            {
+                //Assert
+                StringAssert.Contains(e.Message, BankAccount.CreditAmountLessThanZeroMessage);
+                
+            }
+            
+
+            //Tili‰ ei ole olemassa
+
+            BankCustomer customer3 = new BankCustomer("Mr. Broke");
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => customer3.RemoveAccount(0));
+
+
+            //Transaction:
+            //tilien tiedot: ensimm‰isell‰ asiakkaalla on ensimm‰isell‰ tilill‰ 7.44, toisessa 1.99, toisella asiakkaalla ainoalla tilill‰ 31.99
+            //l‰hetet‰‰n rahaa asiakkaalta toiselle
+            double expectedAmountLeft = 6.44;
+            double expectedAmountReceived = 32.99;
+            double takenAmount = 1.00;
+
+
+            try
+            {
+                BankAccount.SendMoney(customer.CustomerAccount[0], customer2.CustomerAccount[0], takenAmount);
+            }
+            catch (Exception ex)
+            {
+                StringAssert.Contains(ex.Message, BankAccount.BalanceAmountLessThanNeededMessage);
+                
+            }
+            actual = customer.CustomerAccount[0].Balance;
+            double actual2 = customer2.CustomerAccount[0].Balance;
+
+            Assert.AreEqual(expectedAmountLeft, actual);
+            Assert.AreEqual(expectedAmountReceived, actual2);
+
+            //Transaction fail
+            
+
+             takenAmount = 20;
+
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => BankAccount.SendMoney(customer.CustomerAccount[0], customer.CustomerAccount[1], takenAmount));
+
+
+
         }
 
 
-    }
+        }
     }
